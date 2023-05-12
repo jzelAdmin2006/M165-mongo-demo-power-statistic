310c70e0922046691f7e1fc1014014e04d22e170
Johannes Zeller <johannes_zeller@sluz.ch>
2023-05-12 10:57:35 +0200

    solve task 2

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import datetime
+import os
+import time
 
 import psutil
+from pymongo import MongoClient
 
 
 class Power:
@@ -9,3 +12,17 @@ class Power:
         self.ram_total = psutil.virtual_memory().total
         self.ram_usage = psutil.virtual_memory().used
         self.timestamp = datetime.datetime.now() if timestamp is None else timestamp
+
+
+def log_power_usage(db):
+    power = Power()
+    db.power_usage.insert_one(power.__dict__)
+
+
+connection_string = os.environ['MONGODB_CONNECTION_STRING']
+client = MongoClient(connection_string)
+db = client["power_statistics"]
+
+while True:
+    log_power_usage(db)
+    time.sleep(1)
